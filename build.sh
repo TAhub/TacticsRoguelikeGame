00713eeeb18d4d8b62239fef6af7700decf19076
Theodore Abshire <TheodoreAbshire@Gmail.com>
2022-01-09 23:25:27 -0800

    Made the build script "npm install" if needed.

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Get NPM dependencies for first-time calls.
+if [ ! -d "./node_modules" ] ; then
+  npm install
+fi
+
 # Get the output name.
 OUTPUTNAME="out/build.js"
 
@@ -8,52 +13,52 @@ BASEARGUMENTS="-jar compiler.jar"
 BASEARGUMENTS+=" --js=debugFlags.js"
 BASEARGUMENTS+=" --js='src/main.js'"
 for file in src/*js; do
-  if [ $file != 'src/main.js' ]; then
+  if [ $file != 'src/main.js' ] ; then
     BASEARGUMENTS+=" --js="
     BASEARGUMENTS+=$file
   fi
 done
-if [ "$1" = "compiled" ]; then
+if [ "$1" = "compiled" ] ; then
   BASEARGUMENTS+=" --define='DEBUG=false'"
   BASEARGUMENTS+="  --compilation_level ADVANCED_OPTIMIZATIONS"
 else
   BASEARGUMENTS+=" --define='DEBUG=true'"
   BASEARGUMENTS+=" --js='debug_src/**.js'"
 fi;
-if [ "$1" = "compiled" ] || [ "$1" = "check" ]; then
+if [ "$1" = "compiled" ] || [ "$1" = "check" ] ; then
   BASEARGUMENTS+=" --js='extern/**.js'"
 fi;
 BASEARGUMENTS+=" --warning_level=VERBOSE"
 BASEARGUMENTS+=" --language_in=ECMASCRIPT_NEXT"
 
 # Execute the given command.
-if [ "$1" = "check" ]; then
+if [ "$1" = "check" ] ; then
   echo "Testing files..."
   java $BASEARGUMENTS --checks_only
   echo "Running linter..."
   eslint src/* debug_src/* --fix
-elif [ "$1" = "uncompiled" ]; then
+elif [ "$1" = "uncompiled" ] ; then
   echo "Copying into $OUTPUTNAME in uncompiled mode..."
   rm $OUTPUTNAME
   cat releaseFlags.js >> $OUTPUTNAME;
   cat src/main.js >> $OUTPUTNAME;
   for file in src/*js; do
-    if [ $file != 'src/main.js' ]; then
+    if [ $file != 'src/main.js' ] ; then
       cat $file >> $OUTPUTNAME;
     fi
   done
-elif [ "$1" = "debug" ]; then
+elif [ "$1" = "debug" ] ; then
   echo "Copying into $OUTPUTNAME in uncompiled mode with debug..."
   rm $OUTPUTNAME
   cat debugFlags.js >> $OUTPUTNAME;
   cat src/main.js >> $OUTPUTNAME;
   for file in src/*js; do
-    if [ $file != 'src/main.js' ]; then
+    if [ $file != 'src/main.js' ] ; then
       cat $file >> $OUTPUTNAME;
     fi
   done
   for file in debug_src/*js; do cat $file >> $OUTPUTNAME; done
-elif [ "$1" = "compiled" ]; then
+elif [ "$1" = "compiled" ] ; then
   echo "Building into $OUTPUTNAME in compiled mode..."
   java $BASEARGUMENTS --js_output_file "$OUTPUTNAME"
 else
