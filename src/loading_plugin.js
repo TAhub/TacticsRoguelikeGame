bb03bc056e6b2cc6c52946316d2b41265f8ec420
Theodore Abshire <TheodoreAbshire@Gmail.com>
2022-01-10 21:26:34 -0800

    Made a less epilepsy-inducing loading animation.

--- src/loading_plugin.js
+++ src/loading_plugin.js
@@ -13,7 +13,7 @@ class LoadingPlugin extends GamePlugin {
   update(elapsed) {
     if (DEBUG) debugTrackTime('LoadingPlugin.update');
 
-    this.timer = (this.timer + elapsed * 0.5) % 1;
+    this.timer += elapsed * 0.15;
     if (this.switchToPlugin && this.plugin) this.switchToPlugin(this.plugin);
 
     if (DEBUG) debugTrackTimeDone();
@@ -23,10 +23,33 @@ class LoadingPlugin extends GamePlugin {
   draw2D(ctx) {
     if (DEBUG) debugTrackTime('LoadingPlugin.draw');
 
-    const hsv = new HSV(this.timer, 0.5, 0.75);
-    ctx.fillStyle = constructColorHSV(hsv);
-    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    // TODO: real draw...?
+    const x = ctx.canvas.width / 2;
+    const y = ctx.canvas.height / 2;
+    const r = Math.min(x, y);
+    /**
+     * @param {string} colorName
+     * @param {number} rAdd
+     * @param {number} aS
+     * @param {number} aAdd
+     */
+    const drawCircle = (colorName, rAdd, aS, aAdd) => {
+      ctx.fillStyle = data.getColorByNameSafe(colorName);
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+      ctx.arc(x, y, r + rAdd, aS * 2 * Math.PI, (aS + aAdd) * 2 * Math.PI);
+      ctx.lineTo(x, y);
+      ctx.fill();
+    };
+
+    drawCircle('tile border', 10, 0, 1);
+    const t = this.timer * 7;
+    if (t % 2 < 1) {
+      drawCircle('tile selected', 0, 0, 1);
+      drawCircle('tile selected over', 0, this.timer, (t % 2));
+    } else {
+      drawCircle('tile selected over', 0, 0, 1);
+      drawCircle('tile selected', 0, this.timer, (t % 2) - 1);
+    }
 
     if (DEBUG) debugTrackTimeDone();
   }
